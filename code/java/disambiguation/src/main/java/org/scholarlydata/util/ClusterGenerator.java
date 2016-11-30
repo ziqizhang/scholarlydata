21a47b49ccab761918bb5386d2af4bb1dd9962ef
zqz <ziqizhang.email@gmail.com>
2016-11-30 10:37:53 +0000

    .

--- code/java/disambiguation/src/main/java/org/scholarlydata/util/ClusterGenerator.java
+++ code/java/disambiguation/src/main/java/org/scholarlydata/util/ClusterGenerator.java
@@ -16,29 +16,39 @@ public class ClusterGenerator {
 
     public static void main(String[] args) throws IOException {
         ClusterGenerator gen = new ClusterGenerator();
-        Collection<Set<String>> clusters=gen.createCluster(args[0], Integer.valueOf(args[1]), Integer.valueOf(args[2]));
+        Collection<Set<String>> clusters = gen.createCluster(args[0],
+                Integer.valueOf(args[1]), Integer.valueOf(args[2]),
+                args[3]);
         System.out.println("end");
     }
 
-    public Collection<Set<String>> createCluster(String csvFile, int colId1, int colId2) throws IOException {
-        Reader in = new FileReader(csvFile);
-        CSVParser parser = CSVFormat.EXCEL.parse(in);
+    public Collection<Set<String>> createCluster(String inputPairCSV, int colId1, int colId2,
+                                                 String predictionCSV) throws IOException {
+        Reader inPairCSVFile = new FileReader(inputPairCSV);
+        CSVParser parser = CSVFormat.EXCEL.parse(inPairCSVFile);
         Map<String, Set<String>> clusters = new HashMap<>();
         List<CSVRecord> records = parser.getRecords();
-        for(int i=1; i< records.size(); i++) {
-            CSVRecord rec=records.get(i);
-            String id1 = rec.get(colId1).trim();
-            String id2=rec.get(colId2).trim();
-            mergeToCluster(id1, id2, clusters);
+
+        Reader inPredictionCSVFile = new FileReader(predictionCSV);
+        CSVParser parserPrediction = CSVFormat.EXCEL.parse(inPredictionCSVFile);
+        List<CSVRecord> predictions = parserPrediction.getRecords();
+        for (int i = 1; i < records.size(); i++) {
+            String label = predictions.get(i - 1).toString();
+            if (label.equalsIgnoreCase("1")) {
+                CSVRecord rec = records.get(i);
+                String id1 = rec.get(colId1).trim();
+                String id2 = rec.get(colId2).trim();
+                mergeToCluster(id1, id2, clusters);
+            }
         }
         return clusters.values();
     }
 
-    public void mergeToCluster(String id1, String id2, Map<String, Set<String>> clusters){
+    public void mergeToCluster(String id1, String id2, Map<String, Set<String>> clusters) {
         Set<String> cluster = clusters.get(id1);
-        if(cluster==null) {
-            cluster=clusters.get(id2);
-            if(cluster==null)
+        if (cluster == null) {
+            cluster = clusters.get(id2);
+            if (cluster == null)
                 cluster = new HashSet<>();
         }
         cluster.add(id1);
